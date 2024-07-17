56f5e889a221d893250eb7149fbb813b2ef947ff
Julian Swerdlin <julianswer36@gmail.com>
2024-07-17 11:21:54 -0300

    base

---
+++ analista-transacciones/analista.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello world -- Analista" << endl;
+    
+    return 0;
+}
---
+++ gestor-clientes/gestor.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello world -- Gestor" << endl;
+    
+    return 0;
+}
