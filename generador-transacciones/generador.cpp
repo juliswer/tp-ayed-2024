d077aa074278cdcb510bc32691cb60a96ff9a0f1
luc0x <lucascrespo.py@gmail.com>
2024-07-27 09:51:39 -0300

    obtener_usuario() definido

--- generador-transacciones/generador.cpp
+++ generador-transacciones/generador.cpp
@@ -7,7 +7,7 @@ struct Transaccion{
     int monto;
     int fecha;
     bool esEgreso;
-}
+};
 
 //hacer funcion que duplica el tamaño array
 
@@ -18,11 +18,9 @@ struct Usuario{
     int dni;
     float saldo;    
     Transaccion transacciones[];
-}
+};
 
 bool validar_usuario(string username, string password);
-
-Usuario obtener_usuario(string username);
 
 bool eliminar_transaccion(string username, int id);
 
@@ -34,11 +32,11 @@ void realizar_transaccion(string username, int monto, bool esEgreso, int fecha);
 
 Transaccion crear_transaccion(int monto, int fecha, bool esEgreso);
 
-void agregar_transaccion_archivo(string username,Transaccion transaccion); //insertar ordenado por fecha???????
-
 bool verificar_saldo(string username,int monto);
 
-
+// Archivos
+Usuario obtener_usuario(string username, int& errcode);
+void agregar_transaccion_archivo(string username,Transaccion transaccion); //insertar ordenado por fecha???????
 
 /*
 array duplicarArray(Usuario usuarios[], int n){
@@ -51,5 +49,38 @@ array duplicarArray(Usuario usuarios[], int n){
 
 int main() {
   cout << "Hello World!";
+  
   return 0;
+}
+
+// ::Archivos::
+
+/*
+obtener_usuario(string, int) -> Usuario
+
+¿Esta el usuario?
+    SI -> devuelve el usuario
+    NO -> devuelve usuario nulo (todo cero)
+IMPORTANTE: Si no se puede abrir el archivo devuelve usuario nulo con la variable errcode = 1
+*/
+Usuario obtener_usuario(string username, int& errcode)
+{
+    FILE* file = fopen("usuarios.bin", "rb");
+    Usuario user;
+    errcode = 1;
+
+    if (file == NULL)
+        return { 0 };
+      
+    errcode = 0;
+    while (fread(&user, sizeof(user), 1, file))
+    {
+        if(user.username == username)
+        {
+            fclose(file);
+            return user;
+        }
+    }
+    fclose(file);
+    return { 0 }; 
 }
