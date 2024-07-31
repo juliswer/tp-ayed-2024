f80784d54a17e529a9e4927c1a16376a28f7e128
tobato <messi@gmai.com>
2024-07-31 17:56:13 -0300

    verifica que el saldo sea positivo y que sea posible un egreso en caso contrario dice que no le alcanza

--- generador-transacciones/generador.cpp
+++ generador-transacciones/generador.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 struct Transaccion{
     int id;
-    int monto;
+    float monto;
     int fecha;
     bool esEgreso;
 };
@@ -24,15 +24,17 @@ bool validar_usuario(string username, string password);
 
 bool eliminar_transaccion(string username, int id);
 
-void actualizar_saldo(string username);//deberia hacer que la sumatoria de todos los montos sea positiva
+float actualizar_saldo(string username);//deberia hacer que la sumatoria de todos los montos sea positiva
 
-bool sumatoria_tansacciones(Usuario usuario);
+int sumatoria_tansacciones(Usuario usuario);
 
 void realizar_transaccion(string username, int monto, bool esEgreso, int fecha);
 
 Transaccion crear_transaccion(int monto, int fecha, bool esEgreso);
 
-bool verificar_saldo(string username,int monto);
+bool verificar_saldo(string username);
+
+
 
 // Archivos
 Usuario obtener_usuario(string username, int& errcode);
@@ -83,4 +85,55 @@ Usuario obtener_usuario(string username, int& errcode)
     }
     fclose(file);
     return { 0 }; 
+}
+
+
+
+/*
+Actualizar saldo
+    1. Obtener el usuario
+    2. Verificar si el saldo es suficiente
+    3. Actualizar el saldo
+    4. Guardar el usuario
+    5. Devolver el saldo actualizado
+    sino es suficiente devolver -1
+*/
+
+float actualizar_saldo(string username){
+        Usuario user = obtener_usuario(username.0);
+        if(verificar_saldo(username)){
+            user.saldo = sumatoria_tansacciones(user) + user.saldo;
+            cout<<"Transaccion realizada con exito";
+            return user.saldo;
+        }
+        else{
+            cout<<"No se puede realizar esta accion, saldo insuficiente";
+            return -1
+        }
+        
+}//deberia hacer que la sumatoria de todos los montos sea positiva
+
+bool verificar_saldo(string username){
+    Usuario user = obtener_usuario(username,0);
+        if(sumatoria_tansacciones(user) + user.saldo<0){
+            return false;
+        }
+        if(sumatoria_tansacciones(user) + user.saldo >=0){
+            return true;
+        }
+}
+
+int sumatoria_tansacciones(Usuario usuario){
+    int SaldoEstimado=0;
+     int ArrLEN = sizeof(user.transacciones)/sizeof(user.transacciones[0]);
+        for (int i = 0; i < ArrLEN; i++)
+        {
+            if(user.transacciones[i].esEgreso){
+                SaldoEstimado-=user.transacciones[i].monto;
+            }
+            if(user.transacciones[i].esEgreso==false){
+                SaldoEstimado+=user.transacciones[i].monto;
+            }
+        }
+        return SaldoTotal;
 }
